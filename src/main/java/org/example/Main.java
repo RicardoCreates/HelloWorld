ce14b24f9cc5491114585053face5f22e9e480b3
Ricardo Lemser <ricardo.lemser@gmail.com>
2024-08-15 16:43:50 +0200

    done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,7 +13,7 @@ public class Main {
 
         System.out.println("Sum: " +sum);
 
-        // Step 1: Arithmetic Operations with int values
+        // Arithmetic Operations with int values
         int x = 15;
         int y = 4;
 
@@ -23,20 +23,20 @@ public class Main {
         int multiplication = x * y;
         int division = x / y;
 
-        // Print results for int values
+        // Print results
         System.out.println("Arithmetic operations with int values:");
         System.out.println("Addition: " + addition);
         System.out.println("Subtraction: " + subtraction);
         System.out.println("Multiplication: " + multiplication);
         System.out.println("Division: " + division);
 
-        // Step 2: Relational Operators with int values
+
         System.out.println("\nRelational operations with int values:");
         System.out.println("x > y: " + (x > y));
         System.out.println("x < y: " + (x < y));
         System.out.println("x == y: " + (x == y));
 
-        // Step 3: Arithmetic Operations with float and double values
+        // Arithmetic Operations with float and double values
         float a = 15.5f;
         float b = 4.2f;
         double p = 15.5;
@@ -68,7 +68,7 @@ public class Main {
         System.out.println("Multiplication: " + doubleMultiplication);
         System.out.println("Division: " + doubleDivision);
 
-        // Step 4: Relational Operators with float and double values
+        // Relational Operators with float and double values
         System.out.println("\nRelational operations with float values:");
         System.out.println("a > b: " + (a > b));
         System.out.println("a < b: " + (a < b));
